25db0c915ccc14913fe599133599be4b912ae665
于兵 <bing.yu@emaxdata.com>
2019-03-19 11:26:56 +0800

    更新

--- AudioSpectrum02/lineView.h
+++ AudioSpectrum02/lineView.h
@@ -17,9 +17,42 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic,strong)NSMutableDictionary *IDDic;
 @property (nonatomic,strong)NSMutableArray *lineLayerArr;
 
+/**
+ 返回一个CAShapeLayer的圆型图。
+
+ @param point 圆心
+ @param Width 半径
+ @return 返回一个CAShapeLayer的圆型图。
+ */
 -(CAShapeLayer *)drawarcwith:(CGPoint)point withWidth:(CGFloat)Width;
+
+/**
+ 添加一个柱形图
+
+ @param arr 储存着柱形图的长度的数组
+ @param point 圆心
+ @param Width 圆的宽度
+ */
 -(void )drawLineWithArry:(NSArray *)arr with:(CGPoint)point withWidth:(CGFloat)Width;
+
+/**
+ 添加一个折线图
+
+ @param arr 存储着折线图坐标的数组
+ @param point 圆心
+ @param Width 圆的宽度
+ @param ID 折线ID
+ */
 -(void)drawArcWithArry:(NSArray *)arr with:(CGPoint)point withWidth:(CGFloat)Width withID:(NSString *)ID;
+
+/**
+ 添加两个有链接的折线图
+
+ @param arr 存储着折线图1坐标的数组
+ @param arr2 存储着折线图2坐标的数组
+ @param point 圆心
+ @param Width 圆的宽度
+ */
 -(void)drawArcWithArry1:(NSArray *)arr WithArry2:(NSArray *)arr2 with:(CGPoint)point withWidth:(CGFloat)Width;
 -(void)removelayer;
 @end
